638473e64dd20e9574f5d3e64dff84de0f1d9faf
GUELLE Clement <153915554+betonfixe@users.noreply.github.com>
2024-05-21 21:12:37 +0200

    arborescence

---
+++ code/metier/Plateau.java
@@ -0,0 +1,32 @@
+package code.metier;
+
+public class Plateau
+{
+
+	/*-------------*/
+	/*   Donnee    */
+	/*-------------*/
+
+	private int[int[][]][int[][]] plateau;
+
+
+
+
+	/*-------------*/
+	/* Instruction */
+	/*-------------*/
+
+
+	public Plateau()
+	{
+		this.plateau = initPlateau();
+	}
+
+
+
+	public int[int[][]][int[][]] initPlateau()
+	{
+		int[int[][]][int[][]] plateau = new int[int[3][3]][int[3][3]]
+	}
+
+}
